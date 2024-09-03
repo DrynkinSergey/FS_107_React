3669dad3579c6caa21cde687d0142f3b01f35e05
Sergey <sergsmile29@gmail.com>
2024-09-03 21:01:18 +0300

    hook rules

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,25 +1,31 @@
 import { useState } from 'react';
 import s from './Counter.module.css';
 
+// 1. Xуки можуть бути використані ТІЛЬКИ всередині компонента ✅
+// 2. НЕ МОЖНА використовувати хуки всередні функцій в компоненті ❌
+// 3. НЕ МОЖНА об'явити хук всереді цикла, іf ❌
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
+  const [step, setStep] = useState(1);
 
   const handleIncreaseCounter = () => {
-    setCounter(counter + 1);
+    setCounter(prev => prev + step);
   };
 
   const handleDecreaseCounter = () => {
     if (counter > 0) {
-      setCounter(counter - 1);
+      setCounter(prev => prev - step);
     }
   };
   const handleReset = () => {
     setCounter(0);
+    setStep(1);
   };
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
         <h1>{counter}</h1>
+        <input type='number' value={step} onChange={e => setStep(+e.target.value)} />
         <div className={s.flex}>
           <button onClick={handleDecreaseCounter} className='btn'>
             minus
