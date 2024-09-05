1c2f2610b3b8d9f64cd9eb4d58bf3c9b1b24a5e9
Sergey <sergsmile29@gmail.com>
2024-09-05 21:07:01 +0300

    cleanup effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import { Counter } from './components/Counter/Counter';
+import Modal from './components/Modal/Modal';
 import { TodoList } from './components/TodoList/TodoList';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
   return (
     <div className='wrapperBG'>
       {/* <Counter /> */}
       <TodoList />
+      <button onClick={openModal}>Open modal</button>
+      {isOpen && (
+        <Modal onClose={closeModal}>
+          <p>Продам холодильник! Ariston</p>
+        </Modal>
+      )}
     </div>
   );
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,46 @@
+import { useEffect } from 'react';
 import s from './Modal.module.css';
-const Modal = ({ children, title = 'Default modal' }) => {
+const Modal = ({ children, title = 'Default modal', onClose }) => {
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = e => {
+      console.log(e.key);
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    const intervalID = setInterval(() => {
+      console.log(new Date().toLocaleTimeString());
+    }, 1000);
+
+    const timeoutID = setTimeout(() => {
+      console.log('BADABUM 🔥');
+    }, 4000);
+
+    return () => {
+      console.log('Мене закрили!');
+      clearInterval(intervalID);
+      clearTimeout(timeoutID);
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
   return (
-    <div className={s.wrapper}>
+    <div onClick={handleBackdropClick} className={s.wrapper}>
       <div className={s.content}>
         <>
           <h1>{title}</h1>
           <hr />
         </>
-        <button className={s.closeBtn}>×</button>
+        <button onClick={onClose} className={s.closeBtn}>
+          ×
+        </button>
         {children}
       </div>
     </div>
