20ca2f4fdc06f607bd47dcbfca436b1d53922e1d
Sergey <sergsmile29@gmail.com>
2024-09-09 23:31:33 +0300

    init m3l1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,4 @@
-import { useState } from 'react';
-import { Counter } from './components/Counter/Counter';
-import Modal from './components/Modal/Modal';
-import { TodoList } from './components/TodoList/TodoList';
-import Voting from './components/Voting';
-
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-  return (
-    <div className='wrapperBG'>
-      {/* <Counter /> */}
-      {/* <TodoList /> */}
-      <button onClick={openModal}>Open modal</button>
-      {isOpen && (
-        <Modal onClose={closeModal}>
-          <p>Продам холодильник! Ariston</p>
-        </Modal>
-      )}
-      <Voting />
-    </div>
-  );
+  return <div>Forms in React</div>;
 };
 export default App;
