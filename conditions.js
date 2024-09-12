802ffa1cfaa26d8b0b54867a470b3f56ddb4d758
Sergey <sergsmile29@gmail.com>
2024-09-12 21:40:20 +0300

    conditions

---
+++ conditions.js
@@ -0,0 +1,78 @@
+// console.log(!5);
+
+// console.log(1 && 5 && 12 && 7);
+
+// console.log(!-1);
+
+// console.log(NaN || 2 || undefined);
+
+// console.log(NaN && 2 && undefined);
+
+// console.log(true && 2 && 3);
+
+// console.log((!1 && 2) || !3);
+
+// console.log(25 || (null && !3));
+
+// console.log(NaN || null || !3 || undefined || 5);
+
+// console.log(NaN || (null && !3 && undefined) || 5);
+// console.log((5 === 5 && 3 > 1) || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+//   console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger && cola) || (fries === 3 && nuggets)) {
+//   console.log('Done!');
+// }
+
+// console.log(1 || 0);
+// console.log(0 || 1);
+// console.log('' || 1);
+// console.log(undefined || 2);
+// console.log(false || 2);
+// console.log('=================');
+// console.log(1 ?? 0);
+// console.log(0 ?? 1);
+// console.log('' ?? 1);
+// console.log(NaN ?? 2);
+// console.log(undefined ?? 2);
+// console.log(null ?? 2);
+// console.log(false ?? 2);
+
+// console.log(!![].length);
+
+// console.log(1 ?? 0);
+
+// console.log(0 ?? 1);
+// console.log(0 || 1);
+
+// console.log(!!21);
+
+// const result = 0 ?? 1;
+// if (result === 1) {
+//   console.log('Result 1');
+// }
+// if (result === 0) {
+//   console.log('Result 0');
+// }
