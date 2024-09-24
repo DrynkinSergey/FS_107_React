cba297fcee2c2184c2c6333aa0f1ef6c7f082a97
Sergey <sergsmile29@gmail.com>
2024-09-24 20:12:46 +0300

    Not Found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import NotFound from './pages/NotFound/NotFound';
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<h1>Homepage</h1>} />
-        <Route path='/about' element={<h1>About page</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        {/*Якщо роут не підійшов - переведе на сторінку 404 */}
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
