cddadd30eb4bab349148db6b6bc29bc542616e7c
Sergey <sergsmile29@gmail.com>
2024-10-01 20:24:52 +0300

    install redux and configure it

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import App from './App.jsx';
 import './index.css';
 import 'modern-normalize';
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 createRoot(document.getElementById('root')).render(
-  <>
+  <Provider store={store}>
     <App />
     <Toaster />
-  </>
+  </Provider>
 );
---
+++ src/redux/counter/reducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+  counter: 0,
+  step: 1,
+};
+
+export const counterReducer = (state = initialState, action) => {
+  return state;
+};
